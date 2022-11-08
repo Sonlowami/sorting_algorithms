f6773f2d163d0f87856f4c608ff5d53425ca8ec3
Sonlowami <uwilowami64@outlook.com>
2022-11-08 22:48:13 +0200

    Implement selection_sort efficiently

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,24 +9,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int min, tmp;
+	size_t i, j, min;
+	int tmp;
 
 	if (!array || size <= 1)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		min = array[i];
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < min)
-			{
-				tmp = min;
-				min = array[j];
-				array[j] = tmp;
-			}
+			if (array[j] < array[min])
+				min = j;
 		}
-		array[i] = min;
-		print_array(array, size);
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
 	}
 }
